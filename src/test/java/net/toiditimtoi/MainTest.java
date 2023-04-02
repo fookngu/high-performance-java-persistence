a6022c685e7084af17cdca381d7c2f7049d5afa2
Kevin Nguyen <kevin.nguyen@contour.network>
2023-04-02 19:06:13 +0800

    initial commit

---
+++ src/test/java/net/toiditimtoi/MainTest.java
@@ -0,0 +1,13 @@
+package net.toiditimtoi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void testDependencies() {
+        assertEquals(1,  2 - 1);
+    }
+}
