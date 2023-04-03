e28ea54148474481a2927b2c09cb1f66fb380124
Kevin Nguyen <kevin.nguyen@contour.network>
2023-04-03 10:20:01 +0800

    A very common use case of jdbc in general: - adding postgresql driver - common connection acquisition, statement creation and execution of query

---
+++ src/test/java/net/toiditimtoi/jdbc/GeneralTest.java
@@ -0,0 +1,30 @@
+package net.toiditimtoi.jdbc;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.*;
+
+public class GeneralTest {
+    private static final String url = "jdbc:postgresql://localhost:5433/dummy";
+    private static final String userName = "postgres";
+    private static final String password = "mysecretpassword";
+
+    @Test
+    public void summaryExample() throws SQLException {
+        // before doing any interaction with the database, we need to establish a connection to the database
+        try(Connection connection = DriverManager.getConnection(url, userName, password)) {
+            try(Statement statement = connection.createStatement()) {
+                String sql = "select * from post";
+                try(ResultSet result = statement.executeQuery(sql)) {
+                    while(result.next()) {
+                        int id = result.getInt("id");
+                        String title = result.getString("title");
+                        int version = result.getInt("version");
+                        String output = String.format("Id = %d, title = %s, version = %d", id, title, version);
+                        System.out.println(output);
+                    }
+                }
+            }
+        }
+    }
+}
