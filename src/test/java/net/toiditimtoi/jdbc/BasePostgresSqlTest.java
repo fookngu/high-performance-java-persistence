2dc071bbc237fb862d15a2f6afc7a6708bcab8e5
Kevin Nguyen <kevin.nguyen@contour.network>
2023-04-09 13:33:22 +0800

    - using underscore name generator for junit - clearing the post table after every test

--- src/test/java/net/toiditimtoi/jdbc/BasePostgresSqlTest.java
+++ src/test/java/net/toiditimtoi/jdbc/BasePostgresSqlTest.java
@@ -1,7 +1,23 @@
 package net.toiditimtoi.jdbc;
 
+import org.junit.jupiter.api.AfterEach;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public abstract class BasePostgresSqlTest {
     protected final String USER = "postgres";
     protected final String PASSWORD = "mysecretpassword";
     protected final String URL = "jdbc:postgresql://localhost:5433/dummy";
+
+    protected final String POST_TABLE_NAME = "post";
+
+    @AfterEach
+    public void clearDatabase() throws SQLException {
+        try (var connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            var truncateSql = "TRUNCATE %s".formatted(POST_TABLE_NAME);
+            var stm = connection.createStatement();
+            stm.executeUpdate(truncateSql);
+        }
+    }
 }
